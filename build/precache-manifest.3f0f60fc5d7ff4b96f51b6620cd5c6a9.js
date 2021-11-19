2e82586db8cfa5b37c2b57e7671abf333d3ad4e1
gonwms <gon.williams@gmail.com>
2021-11-19 14:55:27 -0300

    build

---
+++ build/precache-manifest.3f0f60fc5d7ff4b96f51b6620cd5c6a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a585ec24da68a076dacc56a5c309a99d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3540933f6cf538c7dc3",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "7467e4acc6b09cba047f",
+    "url": "/static/js/2.f9402512.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f9402512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3540933f6cf538c7dc3",
+    "url": "/static/js/main.855434a2.chunk.js"
+  },
+  {
+    "revision": "adca927408f1c9a53de5",
+    "url": "/static/js/runtime-main.2a27c9f5.js"
+  }
+]);
