f130572053e34706ee2e2ce10761446a74908f30
gonwms <gon.williams@gmail.com>
2021-11-19 12:30:55 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,84 +1,202 @@
 import React from 'react';
-import { makeStyles } from "@material-ui/core/styles"
-
-import {
-  BrowserRouter as Router,
-  Switch, Route, Link
-} from "react-router-dom";
-
-import {
-  Drawer, List, ListItem,
-  ListItemIcon, ListItemText,
-  Container, Typography,
-} from "@material-ui/core";
-
-import HomeIcon from "@material-ui/icons/Home";
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home';
 import InfoIcon from '@material-ui/icons/Info';
+import Home from './pages/Home';
+import Integraciones from './pages/Integraciones';
+import About from './pages/About';
+
+
+const drawerWidth = 200;
 
 const useStyles = makeStyles((theme) => ({
-  drawerPaper: { width: 'inherit' },
+  root: {
+    display: 'flex',
+
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    // width: '100%',
+    marginLeft: 0,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(3, 0),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 3,
+  },
   link: {
     textDecoration: 'none',
-    color: theme.palette.text.primary
-  }
-}))
+    color: theme.palette.text.primary,
+  },
+}));
 
-function App() {
+const rutas = [
+	{
+  path: '/',
+  name: 'Home',
+  page: Home,
+	ico: HomeIcon,
+}, 
+{
+  path: '/integraciones',
+  name: 'Integraciones',
+  page: Integraciones,
+	ico: InfoIcon,
+}, 
+{
+  path: '/about',
+  name: 'About',
+	page: About,
+  ico: InfoIcon,
+}
+];
+
+
+
+
+export default function App() {
   const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Router>
-      <div style={{ display: 'flex' }}>
-        <Drawer
-          style={{ width: '220px' }}
-          variant="persistent"
-          anchor="left"
-          open={true}
-          classes={{ paper: classes.drawerPaper }}
-        >
-          <List>
-            <Link to="/" className={classes.link}>
-              <ListItem button>
-                <ListItemIcon>
-                  <HomeIcon />
-                </ListItemIcon>
-                <ListItemText primary={"Home"} />
-              </ListItem>
-            </Link>
-            <Link to="/about" className={classes.link}>
-              <ListItem button>
-                <ListItemIcon>
-                  <InfoIcon />
-                </ListItemIcon>
-                <ListItemText primary={"About"} />
-              </ListItem>
-            </Link>
-          </List>
-        </Drawer>
-        <Switch>
-          <Route exact path="/">
-            <Container>
-              <Typography variant="h3" gutterBottom>
-                Home
-              </Typography>
-              <Typography variant="body1" gutterBottom>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-              </Typography>
-            </Container>
-          </Route>
-          <Route exact path="/about">
-            <Container>
-              <Typography variant="h3" gutterBottom>
-                About
-              </Typography>
-              <Typography variant="body1" gutterBottom>
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-              </Typography>
-            </Container>
-          </Route>
+			<Router>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+				elevation={0}
+				style={{backgroundColor:'#ff5900'}} 
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Atené
+          </Typography>
+        </Toolbar>
+      </AppBar>
+			
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+				
+        <List>
+          {rutas.map((ruta) => (
+						<Link to={ruta.path} className={classes.link}>
+            <ListItem button key={ruta.name}>
+              <ListItemIcon>{<ruta.ico/>}</ListItemIcon>
+              <ListItemText primary={ruta.name} />
+            </ListItem>
+						</Link>
+          ))}
+        </List>
+      </Drawer>
+
+      <main style= {{width:'2000px'}}
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+				
+				<Switch>
+					{
+					rutas.map((ruta) => (
+							<Route path={ruta.path} exact component={ruta.page}  />
+          ))
+					}
         </Switch>
-      </div>
-    </Router>
+      </main>
+    </div>
+		</Router>
   );
 }
-
-export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+function Home() {
+  return (
+    <div className='home'>
+       <Container style= {{maxWidth:'1640px'}}>
+        <h1>Home</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est quis temporibus quo eos! Est itaque minus veritatis repudiandae adipisci molestias! Consequuntur fuga quidem, nemo praesentium nisi maiores esse ab nostrum.
+        Ipsum mollitia dolor cumque aspernatur laudantium saepe eos unde aut voluptatem magnam repudiandae ea fugit quasi illum, error, repellat blanditiis iste quod numquam consectetur enim rerum consequuntur aliquam voluptates? Voluptates!
+        Cumque recusandae eveniet et aperiam? Dignissimos iure alias nesciunt quis corporis facere, amet earum provident asperiores totam atque modi exercitationem aspernatur assumenda quidem quos ex itaque mollitia. Distinctio, nemo ex?
+        
+</p>
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
